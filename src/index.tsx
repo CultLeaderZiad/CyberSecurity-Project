26f7a43a6fd652c9306816b1ebb54b427a19d720
ziad sabry <cultleaderziad@gmail.com>
2026-03-07 09:38:54 +0200

    new changes for sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import * as Sentry from "@sentry/react";
 import { ProveLanding } from "./screens/ProveLanding/ProveLanding";
 import { Cybersecurity } from "./screens/Cybersecurity/Cybersecurity";
 import { NordWacht } from "./screens/NordWacht/NordWacht";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true, // Auto collected IP address on events
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  css: {
-    postcss: {
-      plugins: [tailwind()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: "cybersecurity-project",
+        project: "javascript-react",
+        authToken: env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    base: "./",
+    css: {
+      postcss: {
+        plugins: [tailwind()],
+      },
     },
-  },
+    build: {
+      sourcemap: true, // Source map generation must be turned on
+    }
+  };
 });
